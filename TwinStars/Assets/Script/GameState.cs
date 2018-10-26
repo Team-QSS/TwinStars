032aea1a91d470d0ccf86ea0cdcf0af3ebfa27ef
Nanachi <b01077016039@gmail.com>
2018-10-26 19:05:04 +0900

    asdasd

--- TwinStars/Assets/Script/GameState.cs
+++ TwinStars/Assets/Script/GameState.cs
@@ -7,7 +7,7 @@ using System.Runtime.Serialization.Formatters.Binary;
 public class GameState : MonoBehaviour {
 
     //게임이 몇 스테이지인지를 의미함
-    public static int StageLevel = 22;
+    public static int StageLevel = 12;
 
     //게임이 멈추었는지를 의미함. 일시정지 뿐만 아니라 클리어 등도 포함함
     public static bool isGamePaused = false;
---
+++ TwinStars/Assets/Script/Option/FullScreen.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FullScreen : MonoBehaviour {
+
+    int counter = 0;
+	
+    // Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(counter == 0)
+        {
+            Screen.SetResolution(1280, 800, true);
+
+            counter++;
+        }
+        else if(counter == 1)
+        {
+            Screen.SetResolution(640, 480, false);
+            counter--;
+        }
+    }
+}
